111e40f7e9e03a44fd234b14119e202da78882f4
pravinpatil <pravin_patil2@persistent.com>
2022-02-12 10:14:13 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,18 +1,12 @@
 # Add implimentation
 def add(x, y);
-    return x+y
-
+    pass
 # Sub implimentation
 def subtract(x, y);
-    return x-y              # on master branch
-
+    pass
 # Mul implimentation
 def multiply(x, y);
-    return x*y          # on bug456
-
+    pass
 # Div implimentation
 def division(x, y);
-    if y==0:                        # on bug789
-        return divide_by_0_error
-    else:
-        return x/y
+    pass
